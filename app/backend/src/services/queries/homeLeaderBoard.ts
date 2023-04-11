f5d2fe3f7838f65f7fe09e5e903253c7c1d671a3
brenolg <brenolg@yahoo.com.br>
2023-04-11 17:49:03 -0300

    feat: add campos efficiency e goalsbalance

--- app/backend/src/services/queries/homeLeaderBoard.ts
+++ app/backend/src/services/queries/homeLeaderBoard.ts
@@ -34,13 +34,29 @@ SUM(
 
 SUM(matches.away_team_goals) AS goalsFavor,
 
-SUM(matches.home_team_goals) AS goalsOwn
+SUM(matches.home_team_goals) AS goalsOwn,
+
+SUM(matches.away_team_goals - matches.home_team_goals) AS goalsBalance,
+
+FORMAT((SUM(
+  CASE
+  WHEN matches.home_team_goals > matches.away_team_goals THEN 3
+  WHEN matches.home_team_goals = matches.away_team_goals THEN 1
+  ELSE 0
+  END
+) / (COUNT(matches.id) * 3)) * 100, 2) AS efficiency
 
 FROM TRYBE_FUTEBOL_CLUBE.teams AS teams
 INNER JOIN TRYBE_FUTEBOL_CLUBE.matches AS matches
 ON teams.id = matches.home_team_id
 WHERE matches.in_progress = false
-GROUP BY teams.id;
+GROUP BY teams.id
+
+ORDER BY 
+totalPoints DESC,
+totalVictories DESC,
+goalsBalance DESC,
+goalsFavor DESC;
 `;
 
 export default homeQuery;
